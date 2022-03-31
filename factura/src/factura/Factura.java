b6043ec6d605a429d6bf8b10eb0e69de6de7cf1a
Nelga62 <nelga62@educastur.es>
2022-03-31 09:46:12 +0200

    Implementación de los métodos de la clase 'Factura'

--- factura/src/factura/Factura.java
+++ factura/src/factura/Factura.java
@@ -4,5 +4,25 @@ import java.util.Vector;
 
 public class Factura {
 	private Vector<Producto> productos;
+
+	public Factura() {
+		this.productos = new Vector<>();
+	}
+	
+	public void meterProducto(Producto p) {
+		productos.add(p);
+	}
+	
+	public float totalFactura() {
+		float suma = 0;
+		for(Producto p : productos) {
+			suma += p.precioTotal();
+		}
+		return suma;
+	}
+	
+	public float aplicarIva(float iva) {
+		return totalFactura() * (1 + iva);
+	}
 	
 }
