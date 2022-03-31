25e1ac0b8b697900d3a2525bcb24117cf8f6c7a5
Nelga62 <nelga62@educastur.es>
2022-03-31 09:33:52 +0200

    Implementación de los métodos de la clase 'Producto'

--- factura/src/factura/Producto.java
+++ factura/src/factura/Producto.java
@@ -5,4 +5,27 @@ public class Producto {
 	private float precio;
 	private int cantidad;
 	
+	public Producto(String nombre, float precio, int cantidad) {
+		this.nombre = nombre;
+		this.precio = precio;
+		this.cantidad = cantidad;
+	}
+	
+	public float precioTotal() {
+		return precio * cantidad;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Producto [nombre=");
+		builder.append(nombre);
+		builder.append(", precio=");
+		builder.append(precio);
+		builder.append(", cantidad=");
+		builder.append(cantidad);
+		builder.append("]");
+		return builder.toString();
+	}
+	
 }
