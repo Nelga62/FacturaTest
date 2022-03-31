0f5ae39240e00265bee9f4f7fe855aad4def287a
Nelga62 <nelga62@educastur.es>
2022-03-31 09:54:56 +0200

    Implementación de Getters y Setters en la clase 'Producto'

--- factura/src/factura/Producto.java
+++ factura/src/factura/Producto.java
@@ -11,6 +11,30 @@ public class Producto {
 		this.cantidad = cantidad;
 	}
 	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
 	public float precioTotal() {
 		return precio * cantidad;
 	}
